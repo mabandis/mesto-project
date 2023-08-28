28221f728df8d5640e1f49ea5a32bf4fb84cf1f0
mabandis <maband1s@yandex.ru>
2023-08-28 22:29:40 +0400

    update: первые наброски по проекту(28/08/2023)

--- src/components/api.js
+++ src/components/api.js
@@ -1,101 +1,130 @@
-const apiConfig = {
-    address: 'https://nomoreparties.co/v1/plus-cohort-27/',
-    headers: {
-      authorization: '37a7ba40-eb6a-4212-b4ef-3f20a061a16d',
-      'Content-Type': 'application/json'
-    }
+// const apiConfig = {
+//     address: 'https://nomoreparties.co/v1/plus-cohort-27/',
+//     headers: {
+//       authorization: '37a7ba40-eb6a-4212-b4ef-3f20a061a16d',
+//       'Content-Type': 'application/json'
+//     }
+// }
+
+class Api {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
   }
-  
-function checkResponse(res) {
+
+  checkResponse(res) {
     if (res.ok) {
       return res.json();
-    }
-    else {
+    } else {
       return Promise.reject(`Ошибка: ${res.status}`);
     }
   }
 
-const getProfile = (apiConfig) => {
-    return fetch(apiConfig.address + 'users/me', {
-      method: 'GET',
-      headers: apiConfig.headers
-    })
-      .then(res => checkResponse(res))
+  getProfile() {
+    return fetch(`${this.baseUrl}/users/me`, {headers: this.headers})
+    .then(res => { return this.checkResponse(res); })
   }
+}
+
+export const api = new Api ({
+  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-27/',
+  headers: {
+    authorization: '37a7ba40-eb6a-4212-b4ef-3f20a061a16d',
+    'Content-Type': 'application/json'
+  }
+})
 
 
-const editProfile = (nameInput, jobInput, apiConfig) => {
-    return fetch(apiConfig.address + 'users/me', {
-      method: 'PATCH',
-      headers: apiConfig.headers,
-      body: JSON.stringify({
-        name: nameInput.value,
-        about: jobInput.value
-      })
-    })
-      .then(res => checkResponse(res))
-  }
+// function checkResponse(res) {
+//     if (res.ok) {
+//       return res.json();
+//     }
+//     else {
+//       return Promise.reject(`Ошибка: ${res.status}`);
+//     }
+//   }
+
+// const getProfile = (apiConfig) => {
+//     return fetch(apiConfig.address + 'users/me', {
+//       method: 'GET',
+//       headers: apiConfig.headers
+//     })
+//       .then(res => checkResponse(res))
+//   }
+
+
+// const editProfile = (nameInput, jobInput, apiConfig) => {
+//     return fetch(apiConfig.address + 'users/me', {
+//       method: 'PATCH',
+//       headers: apiConfig.headers,
+//       body: JSON.stringify({
+//         name: nameInput.value,
+//         about: jobInput.value
+//       })
+//     })
+//       .then(res => checkResponse(res))
+//   }
   
-const editAvatar = (avatarInput, apiConfig) => {
-    return fetch(apiConfig.address + 'users/me/avatar', {
-      method: 'PATCH',
-      headers: apiConfig.headers,
-      body: JSON.stringify({
-        avatar: avatarInput.value
-      })
-    })
-      .then(res => checkResponse(res))
-  }  
+// const editAvatar = (avatarInput, apiConfig) => {
+//     return fetch(apiConfig.address + 'users/me/avatar', {
+//       method: 'PATCH',
+//       headers: apiConfig.headers,
+//       body: JSON.stringify({
+//         avatar: avatarInput.value
+//       })
+//     })
+//       .then(res => checkResponse(res))
+//   }  
 
   
-const getInitialCards = (apiConfig) => {
-    return fetch(apiConfig.address + 'cards', {
-      method: 'GET',
-      headers: apiConfig.headers
-    })
-      .then(res => checkResponse(res))
-  }
+// const getInitialCards = (apiConfig) => {
+//     return fetch(apiConfig.address + 'cards', {
+//       method: 'GET',
+//       headers: apiConfig.headers
+//     })
+//       .then(res => checkResponse(res))
+//   }
   
   
-const postCard = (elem, apiConfig) => {
-    return fetch(apiConfig.address + 'cards', {
-      method: 'POST',
-      headers: apiConfig.headers,
-      body: JSON.stringify({
-        name: elem.name,
-        link: elem.link,
-      })
-    })
-      .then(res => checkResponse(res))
-  }
+// const postCard = (elem, apiConfig) => {
+//     return fetch(apiConfig.address + 'cards', {
+//       method: 'POST',
+//       headers: apiConfig.headers,
+//       body: JSON.stringify({
+//         name: elem.name,
+//         link: elem.link,
+//       })
+//     })
+//       .then(res => checkResponse(res))
+//   }
   
   
-const deleteCard = (card, apiConfig) => {
-    return fetch(apiConfig.address + `cards/${card._id}`, {
-      method: 'DELETE',
-      headers: apiConfig.headers
-    })
-      .then(res => checkResponse(res))
-  }
+// const deleteCard = (card, apiConfig) => {
+//     return fetch(apiConfig.address + `cards/${card._id}`, {
+//       method: 'DELETE',
+//       headers: apiConfig.headers
+//     })
+//       .then(res => checkResponse(res))
+//   }
 
   
-const putLike = (card, apiConfig) => {
-    return fetch(apiConfig.address + `cards/likes/${card._id}`, {
-      method: 'PUT',
-      headers: apiConfig.headers
-    })
-      .then(res => checkResponse(res))
-  }
+// const putLike = (card, apiConfig) => {
+//     return fetch(apiConfig.address + `cards/likes/${card._id}`, {
+//       method: 'PUT',
+//       headers: apiConfig.headers
+//     })
+//       .then(res => checkResponse(res))
+//   }
   
 
-const deleteLike = (card, apiConfig) => {
-    return fetch(apiConfig.address + `cards/likes/${card._id}`, {
-      method: 'DELETE',
-      headers: apiConfig.headers
-    })
-      .then(res => checkResponse(res))
-  }
+// const deleteLike = (card, apiConfig) => {
+//     return fetch(apiConfig.address + `cards/likes/${card._id}`, {
+//       method: 'DELETE',
+//       headers: apiConfig.headers
+//     })
+//       .then(res => checkResponse(res))
+//   }
 
-export {apiConfig, checkResponse, getProfile, editProfile, editAvatar, getInitialCards, postCard, deleteCard, putLike, deleteLike};
+// export {apiConfig, checkResponse, getProfile, editProfile, editAvatar, getInitialCards, postCard, deleteCard, putLike, deleteLike};
 
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,21 +2,33 @@ import '../pages/index.css';
 import {popups, openPopup, closePopup, profileText, profileName, openAvatarButton, addProfileButton, profileInputText, profileInputName, editProfileButton, avatarSubmitButton, createCardButton, handleProfileFormSubmit, closeProfileButtons, editProfileSubmitButton, handleCardFormSubmit, handleAvatarSubmit, userEditForm, addCardForm, avatarForm, addPopup, editPopup, avatarPopup} from "../components/modal";
 import {templateCardSection, createCard} from "../components/card";
 import {enableValidation} from "../components/validate";
-import {checkResponse, getProfile, getInitialCards, apiConfig} from "../components/api";
+import {api} from "../components/api";
 import {addCard} from "../components/card";
 import {renderingProfile, user} from "../components/utils";
 
-Promise.all([getProfile(apiConfig), getInitialCards(apiConfig)])
-  .then(([userData, cards]) => {
+
+
+Promise.all([api.getProfile()])
+  .then(([userData]) => {
     renderingProfile(userData);
-    const initialCards = Array.from(cards.reverse());
-    initialCards.forEach(elem => {
-      addCard(elem);
-    })
   })
   .catch((err) => {
     console.log(err);
   });
+
+
+
+// Promise.all([api.getProfile(), getInitialCards(apiConfig)])
+//   .then(([userData, cards]) => {
+//     renderingProfile(userData);
+//     const initialCards = Array.from(cards.reverse());
+//     initialCards.forEach(elem => {
+//       addCard(elem);
+//     })
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 
   popups.forEach(popup => {
